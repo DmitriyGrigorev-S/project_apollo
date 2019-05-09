9decc68f456c0d37fba214a27d0c13e295a20307
Dmitriy <solo-jumpstyle@mail.ru>
2019-05-09 09:00:23 +0300

    cards flip

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  var pull = $("#navigation__toggle");
+  var nav = $(".navigation ul");
+
+  $(pull).on("click", function (e) {
+    e.preventDefault();
+    $(nav).slideToggle();
+    $(this).toggleClass('.navigation__toggle-button--active');
+  });
+
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 720 && nav.is(':hidden')) {
+      nav.removeAttr('style');
+    }
+  });
+
+  var w = $(window).width();
+  if (w < 992 ) {
+    $('nav.navigation a').on("click", function(){
+      nav.slideToggle();
+    })
+  }
+
+  $("#top-slider").owlCarousel({
+    singleItem: true,
+    navigation: true,
+    theme: "top-slider-theme",
+    navigationText: ["", ""],
+    slideSpeed: 600,
+  });
+
+  $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+    highlightSelector:"nav a"
+  });
+
+});
+
+
+
